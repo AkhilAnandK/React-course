9a69cfeb5f1e2c9da8d9355af52091345bdddc85
Akhilanand K <kundurthiakhil@gmail.com>
2026-09-25 23:34:41 +0530

    created initial structure for travel check list app

---
+++ 07-Travel-List/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏖️ Packify 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have 20 items on your list, and you already packed 10%</em>
+    </footer>
+  );
+}
+
+export default App;
